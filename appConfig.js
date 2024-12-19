d4635b1d56063f85e7324c7e915093038a3a589c
royalartwork <royalartwork8@gmail.com>
2024-12-19 12:25:04 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "",
-  description: "",
-  keywords: "",
+  title: "New Year Appetizers",
+  tagline: "Kick off your New Year's Eve party with these delicious appetizers! #nye #partyfood #appetizers",
+  description: "Elevate your New Year's Eve spread with these unique and delicious appetizers. #nye #appetizers #foodie",
+  keywords: "new year's eve, appetizers, party food, recipes, easy recipes, party planning, food, appetizer ideas",
   author: "Milyader Muda",
   favicon: "",
   imageCount: 20,
